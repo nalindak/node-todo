03374603727a6bba86bc99a25e78ab13a9797dc2
Nalinda Karunaratna <NalindaKarunaratna@gmail.com>
2016-03-27 21:49:20 +1100

    added add, update and delete api methods

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
     app.get('/api/todos/:uname', function (req, res) {
 
         Todos.find({ username: req.params.uname }, function (err, todos) {
@@ -25,10 +26,32 @@ module.exports = function (app) {
 
         if (req.body.id) {
             Todos.findByIdAndUpdate(req.body.id, {
-                todo: req.body.todo, idDone:
-            });
-        } else {
+                todo: req.body.todo,
+                idDone: req.body.isDone,
+                hasAttachement: req.body.hasAttachement }, function (err, todo) {
+                    if (err) throw err;
+                    else res.send('Success');
+                });
 
+        } else {
+            var newTodo = Todos({
+                username: 'test',
+                todo: req.body.todo,
+                isDone: req.body.isDone,
+                hasAttachement: req.body.hasAttachement
+            });
+            newTodo.save(function (err) {
+                if (err) throw err;
+                else res.send('Success');
+            });
         }
+    });
+
+    app.delete('/api/todo/', function (req, res) {
+
+        Todos.findByIdAndRemove(req.body.id, function (err) {
+            if (err) throw err;
+            else res.send('Success');
+        });
     });
 };
